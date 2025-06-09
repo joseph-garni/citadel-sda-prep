c6c19a52989e6ea3ecd83a637c9d35b9e68578b5
Joseph Garni <joseph.garni@student.manchester.ac.uk>
2025-06-09 13:01:03 +0100

    updated dates for synthetic trading data to use business days -> drill 5: sharpe-ratio, maximum drawdown calculations w/ cumulative returns, rolling maximum, drawdown, and max drawdown

--- day-1-pandas-mastery/financial_calculations.py
+++ day-1-pandas-mastery/financial_calculations.py
@@ -23,7 +23,7 @@ import numpy as np
 
 np.random.seed(42)
 
-dates = pd.date_range('2024-01-01', periods=252) # periods in a trading year
+dates = pd.bdate_range('2024-01-01', periods=252) # periods in a trading year
 symbols = ['AAPL', 'MSTF', 'GOOGL', 'TSLA', 'NVDA', 'JPM', 'XOM', 'PG', 'XLR', 'BP', 'GS']
 sector = ['Tech', 'Tech', 'Tech', 'Tech', 'Tech', 'Finance', 'Energy', 'Consumer', 'Consumer', 'Energy', 'Finance']
 num_shares = ['15000000000', '7410000000', '13100000000', '3200000000', '24000000000', '24600000000', '4370000000', '2480000000', '1180000000', '4200000000', '1100000000']
@@ -41,6 +41,9 @@ for i, symbol in enumerate(symbols):
 
     returns = np.random.normal(mu * dt, sigma * np.sqrt(dt), 252)
     price = starting_prices[i] * np.exp(np.cumsum(returns))
+
+
+
     for j, date in enumerate(dates):
         data.append({
             'symbol':symbol,
@@ -173,9 +176,42 @@ drill4['daily return'] = pd.to_numeric(drill4['daily return'], errors='coerce')
 # use dropna, subset to not remove any rows that do not include price, volume, or num shares data
 drill4 = drill4.dropna(subset=['price', 'volume', 'num_shares'])
 
-# method to remove outlier results
-drill4 = drill4[drill4['daily return'].abs<0.2]
+# method to remove outlier results 
+drill4 = drill4[(drill4['daily return'].abs())<0.2]
 
 print('drill 4:')
 print(drill4)
 
+'''
+Drill 5 - Calculate Key Financial Metrics
+
+Implement:
+- Calculate Sharpe Ratio (risk-adjusted returns)
+- maximum drawdown
+- beta (Correlation to the market)
+- annualised return and volatility
+
+'''
+
+drill5 = df.copy()
+
+# sharpe-ratio
+returns = drill5['daily return'].dropna()
+risk_free_daily = 0.02 / 252 # assuming 2% risk-free rate
+
+excess_returns = returns - risk_free_daily
+sharpe_ratio = excess_returns.mean() / returns.std() * np.sqrt(252) if returns.std() > 0 else 0
+
+# maximum drawdown (%) - what is the worst loss you would get if you bought at the worst price as a % value
+
+cumulative = (1 + returns).cumprod()
+rolling_max = cumulative.expanding().max()
+drawdown = (cumulative - rolling_max) / rolling_max
+max_drawdown = drawdown.min()
+
+# using yfinance for beta calculations
+import yfinance as yf
+
+# downloading S&P 500 data - common market bench
+
+market_data = yf.download('^GSPC', start='2024-01-01', end='2025-01-01')
