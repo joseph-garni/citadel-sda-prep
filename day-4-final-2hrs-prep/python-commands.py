9cfc538a30e9cdb5be3572a4446350681da40774
Joseph Garni <joseph.garni@student.manchester.ac.uk>
2025-06-12 12:45:51 +0100

    added returns calculations using pct_change and monthly and entry period, and rolling average calc (simple ma and volatility based on returns) using rolling().mean() and rolling().std()

--- day-4-final-2hrs-prep/python-commands.py
+++ day-4-final-2hrs-prep/python-commands.py
@@ -1,4 +1,21 @@
 # PANDAS essentials
 
+# RETURNS calculations 
+
+# Daily returns by stock symbol i/.e given price is the price each day
 df['daily returns'] = df.groupby('symbol')['price'].pct_change()
+
+# Monthly returns
+df['monthly returns'] = df.groupby(['symbol', df['date'].dt.to_period('M')])['price'].last().pct_change()
+
+# Total returns over period
+
+df['total return to present'] = ( df.groupby('symbol')['price'].last() / df.groupby('symbol')['price'].first() - 1 )
+
+# ROLLING calculations 
+
+# moving averages 
+
+df['sma_20'] = df.groupby('symbol')['price'].rolling(20).mean()
+df['volatility'] = df.groupby('symbol')['daily_return'].rolling(20).std()
 
