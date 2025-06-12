ea35b9f329b80fe9bf538d4b22ce07ca2539b95a
Joseph Garni <joseph.garni@student.manchester.ac.uk>
2025-06-12 13:42:06 +0100

    adding RANKING STOCKS BY PERFORMANCE SQL commands adding CUMULATIVE CALCUATIONS SQL commands using running total and max drawdown (high_watermark_ in SQL

--- day-4-final-2hrs-prep/sql-commands.sql
+++ day-4-final-2hrs-prep/sql-commands.sql
@@ -25,4 +25,23 @@ SELECT symbol, date, close_price,
     FROM stock_prices;
 
 
-    
+-- RANKING FUNCTIONS 
+
+-- RANK STOCKS BY PERFORMANCE EACH DAY
+
+SELECT date, symbol, daily_return,
+    ROW_NUMBER() OVER (PARTITION BY date ORDER BY daily_return DESC) as rank,
+    RANK() OVER (PARTITION BY sector, date ORDER BY daily_return DESC) as sector_rank
+FROM returns_table;
+
+-- CUMULATIVE CALCUATIONS
+
+-- RUNNING TOTALS AND MAX DRAWDOWN
+
+-- uses high_watermark function to show you maxmimum close price of a symbol over a date
+
+SELECT symbol, date, close_price,
+    SUM(volume) OVER (PARTITION BY symbol ORDER BY date) as cum_volume,
+    MAX(close_price) OVER (PARTITION BY symbol ORDER BY date ROWS UNBOUNDED PRECEDING) as high_watermark
+FROM stock_prices;
+
